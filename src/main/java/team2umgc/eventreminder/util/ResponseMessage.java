305a37a40dafffb160e49c866da58a23f5132b99
Shahyah <dshahyah>
2024-11-07 22:48:08 +0530

    base code adjustment

--- src/main/java/team2umgc/eventreminder/util/ResponseMessage.java
+++ src/main/java/team2umgc/eventreminder/util/ResponseMessage.java
@@ -23,31 +23,4 @@ public class ResponseMessage {
     public static final String CONNECT_EXCEPTION_MSG = "val.connect.exception";
 
 
-    //System Error Messages
-
-    public static final String EMPTY_LANGUAGE_LIST_MSG = "val.empty.language.list";
-
-    public static final String DEVICE_REGISTRATION_SUCCESS_MSG = "val.device.registration.success";
-
-    public static final String DEVICE_ALREADY_REGISTERED_MSG = "val.device.already.registered";
-
-    public static final String SUCCESS_MSG = "val.request.successful";
-
-    public static final String INVALID_REQUEST_DATA_MSG = "val.invalid.request.data";
-
-    public static final String EMPTY_POOL_LIST_MSG = "val.empty.pool.list";
-
-    public static final String EMPTY_POOL_METRICS_MSG = "val.pool.metrics.not.exist";
-
-    public static final String EMPTY_MODE_SETTINGS_MSG = "val.empty.mode.settings";
-
-    public static final String FIRMWARE_VERSION_ALREADY_REGISTERED_MSG = "val.firmware.version.already.registered";
-
-    public static final String FIRMWARE_REGISTRATION_SUCCESS_MSG = "val.firmware.registration.success";
-
-    public static final String NO_FIRMWARE_UPDATE_AVAILABLE_MSG = "val.no.firmware.update.available";
-
-    public static final String FIRMWARE_UPDATE_FAILED_MSG = "Firmware update failed";
-
-
 }
